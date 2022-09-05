08d240e57f6f98252dae0400c60a0ea3f6627b17
Vinicius A N Silva <viniciusansilva@outlook.com>
2022-09-05 12:24:15 -0300

    selecionar/buscar usuario

--- index.js
+++ index.js
@@ -10,23 +10,44 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   //receber parametro url e transformar em JSON
-  const params = url.parse(req.url, true).query;
-  console.log(params);
-  // criar usuario --- cadastrar usario
-  fs.writeFile(
-    "database/" + params.id + ".txt",
-    JSON.stringify(params),
-    function (err) {
-      if (err) throw err;
-      console.log("Saved!");
-    }
-  );
-  //atualizar usuario
-  //deketar usuario
+  let urlParse = url.parse(req.url, true);
+  const params = queryString.parse(urlParse.search);
 
+  // criar usuario --- cadastrar usario
+  if (urlParse.pathname == "criar-usuario") {
+    //receber info usuario
+
+    console.log(params);
+
+    //salvar documento com info do usuario
+    fs.writeFile(
+      "database/" + params.id + ".txt",
+      JSON.stringify(params),
+      function (err) {
+        if (err) throw err;
+        console.log("Saved!");
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("Criado com sucesso");
+      }
+    );
+  }
+
+  //selecionar usuario
+  else if (urlParse.pathname == "/selecionar-usuario") {
+    fs.readFile("database/" + params.id + ".txt", function (err, data) {
+      let resposta = data;
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(resposta);
+      console.log(resposta);
+    });
+  }
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.setHeader("Content-Type", "application/json");
+  res.end("hello world");
+  //deletar usuario
 });
 
 //execução
